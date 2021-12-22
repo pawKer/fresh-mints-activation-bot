7e1c32adb10b20c73787f5b07ffb9d6feaa4034a
Rares <rares.rck96@yahoo.ro>
2021-12-22 13:11:14 +0200

    Completing deactivation logic

--- src/logic.ts
+++ src/logic.ts
@@ -1,28 +1,44 @@
 import { DiscordClient, MongoResultActivationKeys } from "../@types/bot";
 import BotConstants from "./utils/bot-constants";
-
+const deactivateServer = async (
+  serverId: string,
+  client: DiscordClient
+): Promise<void> => {
+  await client.serverSettingsRepo.save(serverId, {
+    activated: false,
+    areScheduledMessagesOn: false,
+  });
+};
 const checkUsersAreStillVerified = async (
   client: DiscordClient
 ): Promise<void> => {
   console.log("Checking all users are still verified...");
   const usedActivationKeys: MongoResultActivationKeys[] | null =
     await client.activationKeysRepo.findAllUsedKeys();
-  if (!usedActivationKeys) return;
+  if (!usedActivationKeys) {
+    console.log(
+      `No used activation keys found or DB call failed. Returning from scheduled job...`
+    );
+    return;
+  }
   const guild = await client.guilds.fetch(BotConstants.TEST_GUILD_ID);
   for (const actKey of usedActivationKeys) {
     const member = await guild.members.fetch(actKey.userId);
+    if (!actKey.serverId) continue;
     if (member.id === BotConstants.OWNER_ID) continue;
+
     if (!member) {
-      // Deactivate
       console.log(
         `User [${actKey.userId}] no longer found. Deactivating server.`
       );
+      await deactivateServer(actKey.serverId, client);
+      continue;
     }
     if (!member.roles.cache.has(BotConstants.VERIFIED_ROLE_ID)) {
-      // Deactivate - set server activated to false, set activation key used to false (or delete?)
       console.log(
         `User [${actKey.userId}] no longer has role. Deactivating server.`
       );
+      await deactivateServer(actKey.serverId, client);
     }
   }
 };
