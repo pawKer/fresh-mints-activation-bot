ff33a4ef20151f22e72bc9efc52bfbd3a07939de
Rares <rares.rck96@yahoo.ro>
2023-09-05 15:31:21 +0100

    Update bot-constants.ts

--- src/utils/bot-constants.ts
+++ src/utils/bot-constants.ts
@@ -1,12 +1,12 @@
 class BotConstants {
-  static readonly OWNER_ID: string = "204731639438376970";
-  static readonly GUILD_ID = "922232906850664488";
-  static readonly TEST_GUILD_ID = "912779325235920936";
-  static readonly TEST_BOT_ID = "922247600999112776";
+  static readonly OWNER_ID = "<OWNER-ID>";
+  static readonly GUILD_ID = "<SERVER-ID>";
+  static readonly TEST_GUILD_ID = "<TEST-SERVER-ID>";
+  static readonly TEST_BOT_ID = "<TEST-BOT-ID>";
   static readonly PROD_BOT_ID = "";
-  static readonly VERIFIED_ROLE_ID = "922238783515156493";
-  static readonly TEST_VERIFIED_ROLE_ID = "922479851624071218";
-  static readonly REDEMPTION_CHANNEL_ID = "923232929168130098";
-  static readonly TEST_REDEMPTION_CHANNEL_ID = "912779325235920940";
+  static readonly VERIFIED_ROLE_ID = "<VERIFIED-ROLE-ID>";
+  static readonly TEST_VERIFIED_ROLE_ID = "<TEST-VERIFIED-ROLE-IDD>";
+  static readonly REDEMPTION_CHANNEL_ID = "<REDEMPTION-CHANNEL-ID>";
+  static readonly TEST_REDEMPTION_CHANNEL_ID = "<TEST-REDEMPTION-CHANNEL-ID>";
 }
 export default BotConstants;
